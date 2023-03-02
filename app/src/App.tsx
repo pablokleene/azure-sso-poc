0130f6f91a74dc41d0d96f9e63880cb775b0ce76
Pablo Lins <pablo.lins@kleene.ai>
2023-03-02 15:21:11 -0300

    first commit

---
+++ app/src/App.tsx
@@ -0,0 +1,46 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+// Custom components
+import NavBar from './NavBar';
+import PublicHome from './PublicHome';
+import PrivateHome from './PrivateHome';
+
+function App() {
+
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  async function getUserInfo() {
+    try {
+
+        const response = await fetch('/.auth/me');
+        const payload = await response.json();
+        const { clientPrincipal } = payload;
+        
+        if(clientPrincipal){
+          setUser(clientPrincipal);
+          userHasAuthenticated(true);
+          console.log(`clientPrincipal = ${JSON.stringify(clientPrincipal)}`);
+        } 
+        
+    } catch (error:any) {
+        console.error('No profile could be found ' + error?.message?.toString());
+    }
+};  
+
+  return (
+    <div className="App">
+      <NavBar user={user}/>
+      <main className="column">
+        { isAuthenticated ? <PrivateHome user={user}/> : <PublicHome /> }
+      </main>
+    </div>
+  )
+}
+
+export default App;
